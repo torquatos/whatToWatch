6b27db87aec10d309ac103cf7682e55a7315b9b9
Torquato <shauanatorquato@outlook.com>
2025-09-23 19:37:30 +0000

    writing the elements of the table - database

--- whatToWatch/managementSystem.py
+++ whatToWatch/managementSystem.py
@@ -7,3 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqkite:///mediaToWatch.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
+
+class mediaToWatch(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nameOfMedia = db.Column(db.String(100), nullable = False)
+    typeOfMedia = db.Column
+    genre = db.Column
+    year = db.Column
+    seasons = db.Column
+    concluded = db.Column
+    whereToWatch = db.Column
+    wasWatched = db.Column
+    sinopse = db.Column
