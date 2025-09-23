8384c8da49f008df4a633ca82e23180b8d108dd4
Torquato <shauanatorquato@outlook.com>
2025-09-23 19:29:30 +0000

    configurating the database - SQLAlchemy: library for SQL - Flask: framework web for python

--- whatToWatch/managementSystem.py
+++ whatToWatch/managementSystem.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import flask_sqlalchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqkite:///mediaToWatch.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
