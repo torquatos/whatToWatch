9d395e78269ce66bae7293b54e8751c68520ffbf
torquatos <226768122+torquatos@users.noreply.github.com>
2025-09-24 12:07:02 -0300

    fixing bugs

--- whatToWatch/managementSystem.py
+++ whatToWatch/managementSystem.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqkite:///mediaToWatch.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mediaToWatch.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
@@ -13,8 +13,8 @@ class mediaToWatch(db.Model):
     nameOfMedia = db.Column(db.String(100), nullable = False)
     typeOfMedia = db.Column(db.String(100), nullable = False)
     genre = db.Column(db.String(100), nullable = True)
-    year = db.Column(db.Integer(4), nullable = True)
-    seasons = db.Column(db.Integer(2), nullable = True)
+    year = db.Column(db.Integer, nullable = True)
+    seasons = db.Column(db.Integer, nullable = True)
     concluded = db.Column(db.Boolean(False), nullable = False)
     whereToWatch = db.Column(db.String(100), nullable = False)
     wasWatched = db.Column(db.Boolean(False), nullable = False)
