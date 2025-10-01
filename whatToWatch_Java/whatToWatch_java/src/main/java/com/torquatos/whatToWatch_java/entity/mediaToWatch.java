4bf813b7249cd55c8ad66d1b175bde58aca6613e
torquatos <226768122+torquatos@users.noreply.github.com>
2025-10-01 07:39:36 -0300

    adding lombok dependency and plugin on pom.xml and updating to spring boot 3 and java 17

---
+++ whatToWatch_Java/whatToWatch_java/src/main/java/com/torquatos/whatToWatch_java/entity/mediaToWatch.java
@@ -0,0 +1,16 @@
+package com.torquatos.whatToWatch_java.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class mediaToWatch{
+	
+}
